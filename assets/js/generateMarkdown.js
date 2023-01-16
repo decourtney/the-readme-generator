073ede488560e7a3bfb780cf592dd8a27a6f3cad
decourtney <53956789+decourtney@users.noreply.github.com>
2023-01-15 19:46:11 -0600

    basic functionality

--- assets/js/generateMarkdown.js
+++ assets/js/generateMarkdown.js
@@ -1,18 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function getLicenseBadge(license)
+{
+  let requestedLicenseBadge = '';
+
+  // Check output of requestedLicenseBadge regarding the use of []
+  switch (license)
+  {
+    case 'CCO':
+      requestedLicenseBadge = '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)';
+      break;
+    case 'GNU':
+      requestedLicenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      break;
+    case 'Hippocratic':
+      requestedLicenseBadge = '[![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg)](https://firstdonoharm.dev)';
+      break;
+    case 'ISC':
+      requestedLicenseBadge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+      break;
+    case 'Unlicense':
+      requestedLicenseBadge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+      break;
+    default:
+      // We've been using MIT as the default license throughout the course
+      requestedLicenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+      break;
+  }
+
+  return requestedLicenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}`;
+function generateMarkdown(data)
+{
+  return `
+    # ${data.title}\n\n
+    ## Description\n
+    ${data.description}\n\n
+    ${data.deployedURL}\n\n
+    ${data.images}\n\n
+    ## Installation\n
+    ${data.installation}\n\n
+    ## Usage\n
+    ${data.usage}\n\n
+    ## License\n
+    ${data.license}`;
 }
+
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,13 +1,117 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const inquirerFileTreeSelection = require('inquirer-file-tree-selection-prompt');
+const chalk = require('chalk');
+const fs = require('fs');
+const generateMarkdown = require('./assets/js/generateMarkdown');
+
+inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
+inquirer.registerPrompt('file-tree-selection', inquirerFileTreeSelection);
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter Your Name:',
+        validate: validateIsValue,
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the Project Title:',
+        validate: validateIsValue,
+    },
+    {
+        type: 'editor',
+        name: 'description',
+        message: 'Enter the Project Description and Save Before Closing the Text Editor (CTRL/CMD S):',
+        default: 'N/A'
+    },
+    {
+        type: 'confirm',
+        name: 'isDeployed',
+        message: 'Is the Project Deployed? (y/n)',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'deployedURL',
+        message: 'Enter the Deployed URL:',
+        validate: validateIsValue,
+        when: (answers) => answers.isDeployed
+    },
+    {
+        type: 'confirm',
+        name: 'isImages',
+        message: 'Insert an Image?',
+        default: false
+    },
+    {
+        type: 'confirm',
+        name: 'isImages',
+        message: 'Insert an Image?',
+        default: false
+    },
+    {
+        root: './',
+        type: 'file-tree-selection',
+        name: 'images',
+        message: 'Select file(s) (Press <space> to select, <a> to toggle all, <i> to invert selection, and <enter> to proceed)',
+        multiple: true,
+        when: (answers) => answers.isImages
+    },
+    {
+        type: 'editor',
+        name: 'installation',
+        message: 'Enter the Installation Instructions and Save Before Closing the Text Editor (CTRL/CMD S):',
+        default: 'N/A'
+    },
+    {
+        type: 'editor',
+        name: 'usage',
+        message: 'Enter the Usage Directions and Save Before Closing the Text Editor (CTRL/CMD S):',
+        default: 'N/A'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select a License',
+        choices: ['Creative Commons License', 'GNU GPL v3', 'Hippocratic License 3.0', 'ICS License', 'Unlicense', 'MIT'],
+        default: 'MIT'
+    }
+
+
+]
+
+// Force the user to submit a value
+function validateIsValue(value)
+{
+    if (!value)
+    {
+        return 'Please enter a value.';
+    }
+    return true;
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data)
+{
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log("Successfully created README file!")
+    );
+}
 
 // TODO: Create a function to initialize app 
-function init() { }
+function init()
+{
+    inquirer.prompt(questions).then((answers) =>
+    {
+        const readmeContent = generateMarkdown(answers);
+
+        writeToFile('./READMEtoo.md', readmeContent);
+    });
+}
 
 // Function call to initialize app
 init();
